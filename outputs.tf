9ef02eed8f754c075e6921e7b3a3c7e67d53cd80
philkim1 <hoylerian2000@yahoo.com>
2024-09-05 17:06:41 -0700

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-# TODO: Define the output variable for the lambda function.
-resource "aws_lambda_function" "test_lambda" {
-    function_name = "greet_lambda"
-    s3_bucket = "aws-phil-bucket"
-    s3_key = "lambda_function.zip"
-    role = aws_iam_role.terraform-lambda.arn
-    handler = "lambda_function.lambda_handler"
-    runtime = "python3.12"
-}
