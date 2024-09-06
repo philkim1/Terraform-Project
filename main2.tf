a700d4bc18fc6f108c1aaf6300b1c0ef3bfc5e94
philkim1 <hoylerian2000@yahoo.com>
2024-09-05 17:07:20 -0700

    Add files via upload

---
+++ main2.tf
@@ -0,0 +1,13 @@
+
+
+resource "aws_iam_role_policy" "lambda_policy" {
+    name = "lambda_policy"
+    role = aws_iam_role.terraform-lambda.id
+    policy = file("lambda_policy.json")
+}
+
+resource "aws_iam_role" "terraform-lambda" {
+    name = "terraform-lambda"
+    assume_role_policy = file("role_policy.json")
+}
+
